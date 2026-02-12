cb1d3eb42748ccc0664f3b2696bb36e6ed12cee0
yonatan-nyo <yonatan.njoto@gmail.comm>
2026-02-12 16:33:02 +0700

    updated default api

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8080";
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:9091/api";
 
 interface AppData {
   name: string;
